5c1943b4794c7cb559ea3baa9c2fc38d252c980a
Adam Cohn <adamcohn88@gmail.com>
2018-05-01 17:57:10 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -27,9 +27,7 @@ class Dog
 
   def save
     if self.id.nil?
-      sql = <<-SQL
-        INSERT INTO dogs (name, breed) VALUES (?, ?);
-      SQL
+      sql = 'INSERT INTO dogs (name, breed) VALUES (?, ?);'
       DB[:conn].execute(sql, self.name, self.breed)
       @id = DB[:conn].execute('SELECT last_insert_rowid() FROM dogs')[0][0]
       self
